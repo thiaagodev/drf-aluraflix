0d102b7a7d2ee1740d69f90c714d88f2905895bc
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-16 18:38:29 -0300

    Testando data do serializer

--- aluraflix/tests/test_serializers.py
+++ aluraflix/tests/test_serializers.py
@@ -22,5 +22,14 @@ class ProgramaSerializerTestCase(TestCase):
         data = self.serializer.data
         
         self.assertEqual(set(data.keys()), set(['titulo', 'tipo', 'data_lancamento', 'likes']))
-        
+   
+    def test_verifica_conteudo_dos_campos_serializados(self):
+        """ Teste que verifica o conteúdo dos campos serializados """
+
+        data = self.serializer.data
+
+        self.assertEqual(data['titulo'], self.programa.titulo)
+        self.assertEqual(data['tipo'], self.programa.tipo)
+        self.assertEqual(data['data_lancamento'], self.programa.data_lancamento)
+        self.assertEqual(data['likes'], self.programa.likes)
 
